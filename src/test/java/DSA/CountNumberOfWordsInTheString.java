38de8d40f86d3627d3f0bbe9b12eceb281751d1a
Vipin Borole <vipinborole7601@gmail.com>
2026-08-29 22:36:12 +0530

    count the words of the string

---
+++ src/test/java/DSA/CountNumberOfWordsInTheString.java
@@ -0,0 +1,15 @@
+package DSA;
+
+public class CountNumberOfWordsInTheString {
+
+
+    public static void main(String[] args) {
+
+
+    String str = "This is a sample string";
+
+    String [] n=str.split(" ");
+
+        System.out.println(n.length);
+    }
+}
