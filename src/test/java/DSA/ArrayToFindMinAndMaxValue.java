2232ab5941d7a8ea20869a8b626b61ebbd9ca173
Vipin Borole <vipinborole7601@gmail.com>
2026-09-18 09:39:06 +0530

    added new code

--- src/test/java/DSA/ArrayToFindMinAndMaxValue.java
+++ src/test/java/DSA/ArrayToFindMinAndMaxValue.java
@@ -1,4 +1,34 @@
 package DSA;
 
 public class ArrayToFindMinAndMaxValue {
+
+
+    public static void main(String[] args) {
+
+
+        int a[] = {2,1,5,1,66,22};
+
+        int max =0;
+        int min =0;
+
+        for(int i=0;i<a.length-1;i++)
+        {
+            if(a[max]>a[i])
+            {
+                max=a[i];
+            }
+        }
+        System.out.println(max);
+
+        for(int i=0;i<a.length-1;i++)
+        {
+            if(a[min]<a[i])
+            {
+                min=a[i];
+            }
+        }
+
+        System.out.println(min);
+
+    }
 }
