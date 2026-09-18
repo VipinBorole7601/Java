8ee82ef046c27d85a35c61b41753cb79719882d6
Vipin Borole <vipinborole7601@gmail.com>
2026-09-18 09:39:44 +0530

    r

--- src/test/java/DSA/ArrayToFindMinAndMaxValue.java
+++ src/test/java/DSA/ArrayToFindMinAndMaxValue.java
@@ -4,13 +4,9 @@ public class ArrayToFindMinAndMaxValue {
 
 
     public static void main(String[] args) {
-
-
         int a[] = {2,1,5,1,66,22};
-
         int max =0;
         int min =0;
-
         for(int i=0;i<a.length-1;i++)
         {
             if(a[max]>a[i])
