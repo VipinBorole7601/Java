7b5e339997d78dfedd127930c7aa7c389ba8f9a2
Vipin Borole <vipinborole7601@gmail.com>
2026-09-18 10:56:51 +0530

    test are added

--- src/test/java/DSA/ArraySwap.java
+++ src/test/java/DSA/ArraySwap.java
@@ -7,32 +7,22 @@ public class ArraySwap {
 
     public static void main(String[] args) {
 
+  int[] a1 = {4,6,7,3};
+  int[] a2 = {1,8,9,5};
 
-        int []a1= {2,4,7,8};
-        int []b2={6,9,1,3};
+  int temp=0;
 
-        int temp=0;
+if(a1.length==a2.length) {
+    for (int i = 0; i <= a1.length - 1; i++) {
 
-        if(a1.length==b2.length)
-        {
+        temp = a1[i];
+        a1[i] = a2[i];
+        a2[i] = temp;
 
-            for(int i=0;i<a1.length-1;i++)
-            {
-                temp=a1[i];
-                a1[i]=b2[i];
-                b2[i]=temp;
-            }
-
-            System.out.println(Arrays.toString(a1));
-            System.out.println(Arrays.toString(b2));
-
-        }
-
-
-
-
-
-
+    }
+}
+  System.out.println(" a2 is swaped into a1 :- "+java.util.Arrays.toString(a1));
+  System.out.println(" a1 is swaped into a2 :- "+java.util.Arrays.toString(a2));
 
 
     }
