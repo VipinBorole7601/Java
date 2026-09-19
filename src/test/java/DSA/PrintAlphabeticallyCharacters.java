a90d7f09b7d351698c9af4d0ba2bb94d3e6494e2
Vipin Borole <vipinborole7601@gmail.com>
2026-09-20 02:25:06 +0530

    added new code

---
+++ src/test/java/DSA/PrintAlphabeticallyCharacters.java
@@ -0,0 +1,28 @@
+package DSA;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
+
+public class PrintAlphabeticallyCharacters {
+
+
+    public static void main(String[] args) {
+
+
+        char a[] = {'t','a','y','e','p'};
+        TreeSet<Character> d = new TreeSet<>();
+        for(char w:a)
+        {
+
+            d.add(w);
+
+        }
+        for(char t:d) {
+            System.out.println(t);
+        }
+
+    }
+
+
+}
