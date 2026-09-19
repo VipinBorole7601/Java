d5ff427ef0b360855a9da2016d2eb19970fdd935
Vipin Borole <vipinborole7601@gmail.com>
2026-09-20 00:42:23 +0530

    added new code

---
+++ src/test/java/DSA/StringWordsReverseNotSentence.java
@@ -0,0 +1,41 @@
+package DSA;
+
+public class StringWordsReverseNotSentence {
+
+
+    public static void main(String[] args) {
+
+
+        String name ="vipin borole";
+
+        String [] aname = name.split(" ");
+
+
+
+        for(String n:aname)
+        {
+            for(int j=n.length()-1;j>=0;j--)
+            {
+
+                System.out.print(n.charAt(j));
+
+            }
+
+            System.out.print(" ");
+
+
+        }
+
+
+
+
+
+       }
+
+
+
+    }
+
+
+
+
