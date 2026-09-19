9c92479de2c20f99590ec8626b1070ee2d1f0540
Vipin Borole <vipinborole7601@gmail.com>
2026-09-19 18:50:25 +0530

    added new

--- src/test/java/DSA/FindSecondMaxNumberFromArray.java
+++ src/test/java/DSA/FindSecondMaxNumberFromArray.java
@@ -9,12 +9,10 @@ public class FindSecondMaxNumberFromArray {
      */
 
     public static void main(String[] args) {
-        //way 1
 
-        int a[] = {10,3,1,5,7,90};
+        int a[] = {10,23,1,5,7,90};
         int max =0;
         int secondMax=0;
-
         for(int i=0;i<=a.length-1;i++)
         {
             if(max<a[i])
@@ -22,35 +20,14 @@ public class FindSecondMaxNumberFromArray {
                 max=a[i];
             }
         }
-
         for(int i = 0; i <= a.length - 1; i++) {
             if(a[i] != max && secondMax < a[i]) {
                 secondMax = a[i];
             }
         }
-
-
         System.out.println(max);
         System.out.println(secondMax);
 
-
-
-        //way 2
-
-        int[] number = {2,38, 44,22, 4, 6, 2, 7, 1};
-        int temp=0;
-
-        for(int i = 0; i < number.length; i++) {
-            for(int j = i + 1; j < number.length; j++) {
-                if(number[i] > number[j]) {
-                    temp = number[i];
-                    number[i] = number[j];
-                    number[j] = temp;
-                }
-            }
-        }
-        int secondMax2 = number[number.length - 2];
-        System.out.println("Second Max: " + secondMax2);
 
 
 
