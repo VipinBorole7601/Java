2499348dde0d2ccc11c35de415da9f3d5fb06664
Vipin Borole <vipinborole7601@gmail.com>
2026-09-20 01:24:19 +0530

    added new code

---
+++ src/test/java/DSA/StringOnlyOneWordReverse.java
@@ -0,0 +1,42 @@
+package DSA;
+
+public class StringOnlyOneWordReverse {
+
+
+    public static void main(String[] args) {
+
+
+        String a = "I love my india";
+
+        String n[] = a.split(" ");
+
+        String rev="";
+
+        for(String c:n) {
+            System.out.print(" ");
+
+            if(c.equalsIgnoreCase("india"))
+            {
+                for(int i=c.length()-1;i>=0;i--)
+                {
+
+                    rev = rev+c.charAt(i);
+
+                }
+                System.out.print(rev);
+
+            } else {
+                System.out.print(c);
+            }
+
+
+
+        }
+
+
+
+    }
+
+
+
+}
